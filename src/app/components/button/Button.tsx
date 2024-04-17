c603ae55b8d2a5fd2e5a11b2d5959668df14fb25
Ives <ivesjundi@gmail.com>
2024-04-16 22:50:26 -0300

    feat :sparkles: Button and Card components

---
+++ src/app/components/button/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+interface ButtonProps {
+    children: React.ReactNode
+}
+
+export const Button = ({ children }: ButtonProps) => {
+    return (
+        <button className="bg-orange-600 p-2 rounded-md">{children}</button>
+    )
+}
---
+++ src/app/components/card/Card.tsx
@@ -0,0 +1,7 @@
+export const Card = ({ children }) => {
+    return (
+        <div className='bg-white rounded-md border-solid border-2 h-32'>
+            {children}
+        </div>
+    )
+}
